ae3fa2df14f52a781b127bc5f390dd05c1e335e0
Biswas Khayargoli <biswas.khayargoli@gmail.com>
2025-04-22 18:16:29 +0545

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -49,12 +49,12 @@ For project inquiries, please contact me directly via Upwork. I handle all clien
           delay: 0.1,
         }}
       >
-        <a
+       👉 <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.upwork.com/freelancers/~01459b52cca83e532c"
           target="_blank"
         >
-        👉 <BiLogoUpwork />
+          <BiLogoUpwork />
         </a>
           </motion.div>
 
